ed34e8073623c84ddb508b471b2d0af4cb9c3cd5
Tino Mager <tino.mager@outlook.de>
2018-01-17 17:17:04 +0000

    added loop for logger with interval 10seconds

--- DHT22Logger.js
+++ DHT22Logger.js
@@ -1,21 +1,30 @@
 var sensor = require('node-dht-sensor');
 var fs = require('fs');
 
-var filename = '[YOURFILENAME]';
+var filename = '[YOURLOGFILE]';
 
-sensor.read(22, 17, function(err, temp, hum)
-	{
-		if(!err){
-			console.log('temp: ' + temp.toFixed(2) + '| hum: ' + hum.toFixed(2));
-			var datastring = new Date().toUTCString() + ',' + temp + ',' + hum;
-			fs.appendFile(filename, datastring, function (err) {
-		  		if (err) {
-					console.log('could not save data to file' + err);
-				}
-		  		console.log('Saved data to file!');				
-			});
-		}
-		else{
-			console.log('error reading values');
-		}
+function logData(){
+	sensor.read(22, 17, function(err, temp, hum)
+		{
+			if(!err){
+				var time = new Date().toUTCString();
+				console.log(time + ' temp: ' + temp.toFixed(2) + '| hum: ' + hum.toFixed(2));
+				var datastring = time + ',' + temp + ',' + hum + '\n';
+				fs.appendFile(filename, datastring, function (err) {
+			  		if (err) {
+						console.log('could not save data to file' + err);
+					}
+			  		console.log('Saved data to file!');				
+				});
+			}
+			else{
+				console.log('error reading values');
+			}
 	});
+}
+
+logData();
+
+setInterval(function(){
+        logData(); 
+    }, 10000);
